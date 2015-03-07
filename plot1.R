4c0be66e9af1b4655f2d4d430f542a825b271c32
Mucio Osorio <mucio.osorio@itson.edu.mx>
2015-03-07 02:29:18 -0700

    Code for plot1

--- plot1.R
+++ plot1.R
@@ -9,6 +9,7 @@ Date == "2/2/2007"| Date=="1/2/2007")
 hist(datos$Global_active_power,main="Global Active Power",xlab="Global Active Power (kilowatts)",
 ylab="Frequency", col="Red")
 
+
 ## Histograma a archivo (Histogram to PNG file)
 dev.copy(png, file="plot1.png", height=480, width=480)
 dev.off()
